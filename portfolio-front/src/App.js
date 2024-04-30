c2946e9f0f3a1a03327bb8eabb32ed210c9d80b4
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-04-30 13:03:56 +0530

    first commit

--- portfolio-front/src/App.js
+++ portfolio-front/src/App.js
@@ -3,6 +3,8 @@ import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import ProfileSideBar from "./components/ProfileSideBar";
 import AboutMe from "./content/AboutMe";
+import Clients from "./content/Clients";
+import Service from "./content/Service";
 import Nav1 from "./mobileResponsive/Nav1";
 
 
@@ -30,108 +32,12 @@ function App() {
                     <AboutMe/>
 
                     {/* what i do section start */}
-                    <div className="pb-12 px-2 sm:px-5 md:px-10 lg:px-14">
-                        <h3 className="text-[35px] dark:text-white font-bold font-robotoSlab pb-5"> What I do! </h3>
-                        <div className="grid gap-8 grid-cols-1 md:grid-cols-2 xl:grid-cols-2">
-                            <div className="about-box bg-[#fcf4ff] dark:bg-transparent">
-                                <img className="w-10 h-10 object-contain block" src="images/icons/icon.svg"
-                                    alt="icon" />
-                                <div className="space-y-2">
-                                    <h3 className="dark:text-white text-[22px] font-semibold"> Ui/Ux Design </h3>
-                                    <p className="leading-8 text-gray-lite dark:text-[#A6A6A6]"> Lorem ipsum dolor sit
-                                        amet, consectetuer adipiscing elit, sed diam euismod volutpat. </p>
-                                </div>
-                            </div>
-
-                            <div className="about-box bg-[#fefaf0] dark:bg-transparent">
-                                <img className="w-10 h-10 object-contain block" src="images/icons/icon1.svg"
-                                    alt="icon" />
-                                <div className="space-y-2">
-                                    <h3 className="dark:text-white text-[22px] font-semibold"> App Development </h3>
-                                    <p className="leading-8 text-gray-lite dark:text-[#A6A6A6]"> Lorem ipsum dolor sit
-                                        amet, consectetuer adipiscing elit, sed diam euismod volutpat. </p>
-                                </div>
-                            </div>
-
-                            <div className="about-box bg-[#fcf4ff] dark:bg-transparent">
-                                <img className="w-10 h-10 object-contain block" src="images/icons/icon2.svg"
-                                    alt="icon" />
-                                <div className="space-y-2">
-                                    <h3 className="dark:text-white text-[22px] font-semibold"> Photography </h3>
-                                    <p className="leading-8 text-gray-lite dark:text-[#A6A6A6]"> Lorem ipsum dolor sit
-                                        amet, consectetuer adipiscing elit, sed diam euismod volutpat. </p>
-                                </div>
-                            </div>
-
-                            <div className="about-box bg-[#fff4f4] dark:bg-transparent">
-                                <img className="w-10 h-10 object-contain block" src="images/icons/icon3.svg"
-                                    alt="icon" />
-                                <div className="space-y-2">
-                                    <h3 className="dark:text-white text-[22px] font-semibold"> Photography </h3>
-                                    <p className="leading-8 text-gray-lite dark:text-[#A6A6A6]"> Lorem ipsum dolor sit
-                                        amet, consectetuer adipiscing elit, sed diam euismod volutpat. </p>
-                                </div>
-                            </div>
-
-                            <div className="about-box bg-[#fff0f8] dark:bg-transparent">
-                                <img className="w-10 h-10 object-contain block" src="images/icons/icon4.svg"
-                                    alt="icon" />
-                                <div className="space-y-2">
-                                    <h3 className="dark:text-white text-[22px] font-semibold"> Managment </h3>
-                                    <p className="leading-8 text-gray-lite dark:text-[#A6A6A6]"> Lorem ipsum dolor sit
-                                        amet, consectetuer adipiscing elit, sed diam euismod volutpat. </p>
-                                </div>
-                            </div>
-
-                            <div className="about-box bg-[#f3faff] dark:bg-transparent">
-                                <img className="w-10 h-10 object-contain block" src="images/icons/icon5.svg"
-                                    alt="icon" />
-                                <div className="space-y-2">
-                                    <h3 className="dark:text-white text-[22px] font-semibold"> Web Development </h3>
-                                    <p className="leading-8 text-gray-lite dark:text-[#A6A6A6]"> Lorem ipsum dolor sit
-                                        amet, consectetuer adipiscing elit, sed diam euismod volutpat. </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    {/* what i do section start */}
-                    <div className="px-2 sm:px-5 md:px-10 lg:px-14">
-                        <div className="bg-[#F8FBFB] dark:bg-[#0D0D0D] max-w-full h-auto py-10 rounded-xl">
-                            <h3 className="text-center dark:text-white text-6xl mb-3 font-semibold"> Clients </h3>
-                            {/* slider and slider items */}
-                            <div className="slickOne text-center px-2 pt-8">
-                                <div>
-                                    <img className="overflow-hidden brand-img" src="images/slider/brand.png"
-                                        alt="brand icon" />
-                                </div>
-                                <div>
-                                    <img className="overflow-hidden brand-img" src="images/slider/brand1.png"
-                                        alt="brand icon" />
-                                </div>
-                                <div>
-                                    <img className="overflow-hidden brand-img" src="images/slider/brand2.png"
-                                        alt="brand icon" />
-                                </div>
-                                <div>
-                                    <img className="overflow-hidden brand-img" src="images/slider/brand3.png"
-                                        alt="brand icon" />
-                                </div>
-                                <div>
-                                    <img className="overflow-hidden brand-img" src="images/slider/brand4.png"
-                                        alt="brand icon" />
-                                </div>
-                                <div>
-                                    <img className="overflow-hidden brand-img" src="images/slider/brand1.png"
-                                        alt="brand icon" />
-                                </div>
-                                <div>
-                                    <img className="overflow-hidden brand-img" src="images/slider/brand1.png"
-                                        alt="brand icon" />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+                    <Service/>
+                    {/* what i do section ends */}
+                    
+                    {/* clients section start */}
+                    <Clients/>  
+                    {/* clients section start */}
 
                 <Footer/>
                 </div>
---
+++ portfolio-front/src/content/Clients.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+const Clients = () => {
+  return (
+    <div className="px-2 sm:px-5 md:px-10 lg:px-14">
+                        <div className="bg-[#F8FBFB] dark:bg-[#0D0D0D] max-w-full h-auto py-10 rounded-xl">
+                            <h3 className="text-center dark:text-white text-6xl mb-3 font-semibold"> Clients </h3>
+                            {/* slider and slider items */}
+                            <div className="slickOne text-center px-2 pt-8">
+                                <div>
+                                    <img className="overflow-hidden brand-img" src="images/slider/brand.png"
+                                        alt="brand icon" />
+                                </div>
+                                <div>
+                                    <img className="overflow-hidden brand-img" src="images/slider/brand1.png"
+                                        alt="brand icon" />
+                                </div>
+                                <div>
+                                    <img className="overflow-hidden brand-img" src="images/slider/brand2.png"
+                                        alt="brand icon" />
+                                </div>
+                                <div>
+                                    <img className="overflow-hidden brand-img" src="images/slider/brand3.png"
+                                        alt="brand icon" />
+                                </div>
+                                <div>
+                                    <img className="overflow-hidden brand-img" src="images/slider/brand4.png"
+                                        alt="brand icon" />
+                                </div>
+                                <div>
+                                    <img className="overflow-hidden brand-img" src="images/slider/brand1.png"
+                                        alt="brand icon" />
+                                </div>
+                                <div>
+                                    <img className="overflow-hidden brand-img" src="images/slider/brand1.png"
+                                        alt="brand icon" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+  )
+}
+
+export default Clients
