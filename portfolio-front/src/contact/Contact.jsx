ea4c81c0b8479fd14705f59b09a1a2182b31f3c2
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-05-08 05:09:03 +0530

    first commit

--- portfolio-front/src/contact/Contact.jsx
+++ portfolio-front/src/contact/Contact.jsx
@@ -109,7 +109,7 @@ const Contact = () => {
       htmlFor="message"
       className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-color-910 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-[#CA56F2] peer-focus:dark:text-[#FF6464] peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-8"
     >
-      Message *
+      What's Your Requirement *
     </label>
   </div>
 
