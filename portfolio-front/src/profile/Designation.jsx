f8c19675deb32ec874f4a251b9cceb55030213d7
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2024-05-24 09:43:36 +0530

    first commit

--- portfolio-front/src/profile/Designation.jsx
+++ portfolio-front/src/profile/Designation.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import { Typewriter } from "react-simple-typewriter";
 
 const Designation = () => {
   return (
     <h3 className="mb-4 text-[#7B7B7B] inline-block dark:bg-[#1D1D1D] px-5 py-1.5 rounded-lg dark:text-[#A6A6A6]">
-                        Web Developer
+      <Typewriter words={["Eat", "Sleep", "Code", "Repeat!"]} loop={5} cursor cursorStyle="_" typeSpeed={70} deleteSpeed={50} delaySpeed={1000} onLoopDone={handleDone} onType={handleType} />
     </h3>
-  )
-}
+  );
+};
 
-export default Designation
+export default Designation;
