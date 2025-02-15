c96a9a60d0d7ceb9e90406be061127676673d1c1
NikhilPathaniaa <developernikhil1@gmail.com>
2025-02-15 12:48:28 +0530

    seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,19 +62,19 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <title>{"Nikhil Pathania - Full Stack Developer Portfolio"}</title>
-        <meta name="description" content={layoutMetadata.description || "Professional portfolio of Nikhil Pathania, a Full Stack Developer specializing in web development, custom software solutions, and API development."} />
-        <meta name="keywords" content={metadata.keywords || "Full Stack Developer, Web Development, Custom Software, API Development, Freelance Developer, Nikhil Pathania"} />
+        <meta name="description" content={layoutMetadata.description || "Professional portfolio of Nikhil Pathania, a Full Stack Developer specializing in web development, custom software solutions, UI/UX design, and Promo videos."} />
+        <meta name="keywords" content={metadata.keywords || "Full Stack Developer, Web Development, Custom Software, UI/UX Design, Promo Videos, Nikhil Pathania"} />
         <meta name="author" content={metadata.author || "Nikhil Pathania"} />
         {/* Open Graph Tags */}
         <meta property="og:title" content={metadata.openGraph.title || "Nikhil Pathania - Full Stack Developer Portfolio"} />
         <meta property="og:description" content={metadata.openGraph.description || "Explore the portfolio of Nikhil Pathania, a Full Stack Developer with expertise in modern web technologies."} />
         <meta property="og:url" content={metadata.openGraph.url || "https://nikhilx.vercel.app"} />
-        <meta property="og:image" content={metadata.openGraph.images[0]?.url || "https://default-image-url.com"} />
+        <meta property="og:image" content={metadata.openGraph.images[0]?.url || "https://nikhilx.vercel.app/assets/banner.jpg"} />
         {/* Twitter Card Tags */}
         <meta name="twitter:card" content={metadata.twitter.card || "summary_large_image"} />
         <meta name="twitter:title" content={metadata.twitter.title || "Nikhil Pathania - Full Stack Developer Portfolio"} />
         <meta name="twitter:description" content={metadata.twitter.description || "Discover the skills and projects of Nikhil Pathania, a Full Stack Developer."} />
-        <meta name="twitter:image" content={metadata.twitter.image || "https://default-twitter-image-url.com"} />
+        <meta name="twitter:image" content={metadata.twitter.image || "https://nikhilx.vercel.app/assets/banner.jpg"} />
       </head>
       <body className={`bg-[url('/background/bg.jpg')] dark:bg-[url('/background/bgtwo.jpg')] min-h-screen bg-no-repeat bg-center bg-cover bg-fixed md:pb-16 w-full ${poppins.variable} ${roboto_slab.variable} antialiased`}>
         <DayNight />
